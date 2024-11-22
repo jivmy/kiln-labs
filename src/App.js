c21c11095dbb5f01126df5a10d4bc0d4575210ab
Jimmy Shan <jimmy@jshan.ca>
2024-11-22 12:34:35 -0800

    name and header

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="container">
+        <div className="content">
+          <h1>
+            <a href="/" className="name">
+              Jimmy Shan
+            </a>
+          </h1>
+          <p className="description">
+            I'm a product designer motivated by making the internet more playful and energetic. SF and NYC.
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
