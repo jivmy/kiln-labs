376091618649f0435a8f63d08996e6d59ec38515
Jimmy Shan <jimmy@jshan.ca>
2024-12-08 04:19:07 -0800

    push

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,29 +1,10 @@
 import React from 'react';
 import './home.css';
 
-function Home() {
-  return (
-    <div className="container">
-      <div className="header-text">
-        Crafting a more playful internet from New York City.
-      </div>
-
-      <div className="grid">
-        <div className="grid-item">
-          <div className="image-placeholder"></div>
-        </div>
-        <div className="grid-item">
-          <div className="image-placeholder"></div>
-        </div>
-        <div className="grid-item">
-          <div className="image-placeholder"></div>
-        </div>
-        <div className="grid-item">
-          <div className="image-placeholder"></div>
-        </div>
-      </div>
-    </div>
-  );
-}
+const Home = () => (
+  <div className="container">
+    <h1 className="header-text">Crafting playful interfaces from New York City</h1>
+  </div>
+);
 
 export default Home;
