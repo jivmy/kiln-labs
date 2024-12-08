3f386efc839a1ee769e83b1fe8d9b918afe8b5e3
Jimmy Shan <jimmy@jshan.ca>
2024-12-08 05:16:46 -0800

    type

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,6 +4,9 @@ import './home.css';
 const Home = () => (
   <div className="wrapper">
     <h1 className="header">Crafting playful interfaces from New York City.</h1>
+    <h1 className="header">Now—running, egg-noodles, morning dew. </h1>
+    <h1 className="header">(310)-806-2199</h1>
+
   </div>
 );
 
