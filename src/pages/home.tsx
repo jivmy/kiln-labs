e2abd388f8cbf70279264fd873e5bd2e80414aa4
Jimmy Shan <jimmy@jshan.ca>
2024-12-08 02:54:03 -0800

    changes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,34 +2,16 @@ import React from 'react';
 import './home.css';
 
 function Home() {
-  const projects = [
-    { label: 'Kalipso', number: '001' },
-    { label: '—', number: '002' },
-    { label: '—', number: '003' },
-    { label: '—', number: '004' },
-    { label: '—', number: '005' },
-    { label: '—', number: '006' },
-    { label: '—', number: '007' },
-    { label: '—', number: '008' },
-    { label: '—', number: '009' }
-  ];
-
   return (
     <div className="container">
       <div className="header-text">
-        Crafting a more playful internet from New York City—some of my creativity lives on this website.
+        Crafting a more playful internet from New York City.
       </div>
 
       <div className="grid">
-        {projects.map((project, index) => (
-          <div key={index} className="grid-item">
-            <div className="image-placeholder"></div>
-            <div className="item-info">
-              <div className="item-number">{project.number}</div> {/* Moved here */}
-              <div className="item-label">{project.label}</div>  {/* Moved here */}
-            </div>
-          </div>
-        ))}
+        <div className="grid-item">
+          <div className="image-placeholder"></div>
+        </div>
       </div>
     </div>
   );
