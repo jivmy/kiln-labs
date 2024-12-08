38237ab826e1f5dbf3a6d0c837822faaf3abe4d3
Jimmy Shan <jimmy@jshan.ca>
2024-12-08 04:33:45 -0800

    nothing

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './home.css';
 
 const Home = () => (
-  <div className="container">
-    <h1 className="header-text">Crafting playful interfaces from New York City</h1>
+  <div className="wrapper">
+    <h1 className="header-text">Crafting playful interfaces from New York City.</h1>
   </div>
 );
 
