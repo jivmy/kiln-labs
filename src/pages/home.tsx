bad57905411a012eb89ca66c4ba57fe1dcc4e994
Jimmy Shan <jimmyshan@Jimmys-MacBook-Pro.local>
2024-12-20 17:26:10 -0500

    add

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,6 +6,9 @@ const Home = () => (
     <h1 className="header">Crafting playful interfaces from New York City.</h1>
     <div className="demo"></div>
     <h1 className="header">Now—running, egg-noodles, morning dew. </h1>
+    <div className="demo"></div>
+    <h1 className="header">Here are some explorations with <a href="https://rive.app  "><u>Rive</u></a></h1>
+    <div className="demo"></div>
   </div>
 );
 
