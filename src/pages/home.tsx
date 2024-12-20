bb34bf24e4a1157bf1f5aa9607f4f714f17da4c9
Jimmy Shan <jimmyshan@Jimmys-MacBook-Pro.local>
2024-12-20 17:08:42 -0500

    push

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import BreathingCircles from '../components/BreathingCircles';
+import '../index.css';
 
-const Home = () => {
-  return (
-    <div className="wrapper">
-      <h1 className="header">Crafting playful interfaces from New York City.</h1>
-      <div 
-        className="demo" 
-        style={{ 
-          position: 'relative',
-          width: '100%',
-          aspectRatio: '1',
-          backgroundColor: 'transparent',
-        }}
-      >
-        <BreathingCircles />
-      </div>
-      <h1 className="header">Now—running, egg-noodles, morning dew. </h1>
-    </div>
-  );
-};
+const Home = () => (
+  <div className="wrapper">
+    <h1 className="header">Crafting playful interfaces from New York City.</h1>
+    <div className="demo"></div>
+    <h1 className="header">Now—running, egg-noodles, morning dew. </h1>
+  </div>
+);
 
 export default Home;
